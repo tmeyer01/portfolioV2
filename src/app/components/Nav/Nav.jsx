918faacb327ae6aa6d3cb68e9cf2f1c42e5ddbeb
ThomasMeyer <tmeyerwebdev@gmail.com>
2023-11-01 13:57:44 -0700

    adds mobile nav icon

--- src/app/components/Nav/Nav.jsx
+++ src/app/components/Nav/Nav.jsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 //components
 import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+// import { useAmp } from "next/amp";
 
 const navLinks = [
   {
@@ -19,12 +24,31 @@ const navLinks = [
 ];
 
 function Nav() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex flex-wrap lg:py-4 items-center justify-between mx-auto px-4 py-2">
         <Link href={"/"} className="text-2xl md:text-3xl text-white">
           &lt;TmDev/&gt;
         </Link>
+        <div className="mobile-menu md:hidden block">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-3 rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-3 rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
