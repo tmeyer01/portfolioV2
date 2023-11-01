bcb81e91fd2b8358e6660d5925c0fddbeed1dfc8
ThomasMeyer <tmeyerwebdev@gmail.com>
2023-11-01 13:24:04 -0700

    adds logo to nav bar

--- src/app/components/Nav/Nav.jsx
+++ src/app/components/Nav/Nav.jsx
@@ -22,8 +22,8 @@ function Nav() {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex flex-wrap lg:py-4 items-center justify-between mx-auto px-4 py-2">
-        <Link href={"/"} className="text-2xl md:text-5xl text-white">
-          TMdev
+        <Link href={"/"} className="text-2xl md:text-3xl text-white">
+          &lt;TmDev/&gt;
         </Link>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
