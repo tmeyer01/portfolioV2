ec5d98c6b99adc1f33f401b9878f5054c380a28d
ThomasMeyer <tmeyerwebdev@gmail.com>
2023-11-04 12:03:22 -0700

    update:  projectData fix links

--- src/app/components/Projects/ProjectData.jsx
+++ src/app/components/Projects/ProjectData.jsx
@@ -1,32 +1,32 @@
 export const projects = [
   {
     id: 1,
-    title: "Travel Chat",
+    title: "Let Jet",
     imgSrc: "/images/projects/letsJet.png",
     description:
-      "A single-page social media application that lets users find friends based on their interests and location. Users can chat with friends via an external powered by Getstream.io. Users can post to their news feed and like friends posts. The project was built with ReactJS front end and Ruby on Rails, Active record and PostgreSQL on the back-end. This was a partnered project and I worked on all aspects of it, including setting up server and database and working on the front end.",
+      "A single-page social media application lets users find friends based on their interests and location. Users can chat with friends via an external powered chat app by Getstream.io. Users can post to their news feeds and like friends' posts. The project was built with ReactJS on the front end, Ruby on Rails, Active Record, and PostgreSQL on the back-end. This was a partnered project, and I worked on all aspects, including setting up the server and database and working on the front-end.",
     techstack:
       "Client side was built with ReactJS and Server side was built with Rails. ",
     link: "https://github.com/tmeyer01/TravelChat",
-    video: "https://www.youtube.com/embed/5hwNdGIJW3g",
+    video: "https://www.youtube.com/embed/8ZMev6Ics_Y?si=qaCDbePfX9v3fWLq",
   },
   {
     id: 2,
     title: "TODO",
     imgSrc: "/images/projects/todo.png",
     description:
-      "A full stack web application that allows users to create a todo list. Except this TODO list is auto categorizing. Users can mark tasks important, remove tasks when finished and move tasks to different categories. This project was built with Javascript, HTML, CSS, SASS on the front end and Node, Express, and SQL on the back-end and utilizes Wolfram alpha API and SERP API. This was a group project and I worked on the server-side code as well the front-end functionality.",
+      "A full-stack web application that allows users to create a to-do list. Except this TODO list is auto-categorizing. Users can mark tasks as important, remove tasks when finished and move tasks to different categories. This project was built with Javascript, HTML, CSS, and SASS on the front end and Node, Express, and SQL on the back end. It utilizes Wolfram alpha API and SERP API. This was a group project, and I worked on the server-side code as well as the front-end functionality.",
     techstack: "Built with Node and Express.",
     link: "https://github.com/tmeyer01/midterm-TODO",
-    video: "https://www.youtube.com/embed/VvufQl1dpRM",
+    video: "https://www.youtube.com/embed/ik9rCSFzXnw?si=RuYOGtfkjg5BreTZ",
   },
   {
     id: 3,
     title: "TinyApp",
     imgSrc: "/images/projects/tinyapp.png",
     description:
-      "A bit.ly clone. This full stack web application allows users to save shortened website URLS. Built with Node and Express",
+      "A bit.ly clone. This full-stack web application allows users to save shortened website URLs. Built with Node and Express",
     link: "https://github.com/tmeyer01/tinyapp",
-    video: "https://www.youtube.com/embed/S7_BBUsAGOU",
+    video: "https://www.youtube.com/embed/eYK4QtlHWqs?si=pD4q6clXjOBtDu6m",
   },
 ];
